7e305b507fcde52150beafc84b72698f26803f76
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-07-14 14:58:45 -0300

    Initial commit

---
+++ src/main/java/eds/cristian/ApiRestfulSpringApplication.java
@@ -0,0 +1,13 @@
+package eds.cristian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eds/cristian/ApiRestfulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package eds.cristian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
