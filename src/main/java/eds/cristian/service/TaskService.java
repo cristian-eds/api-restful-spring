24faaf0e17c0b4323da4b00116a6863794ed5af6
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-07-14 21:37:40 -0300

    Created TaskService findById, create, findAll

---
+++ src/main/java/eds/cristian/service/TaskService.java
@@ -0,0 +1,15 @@
+package eds.cristian.service;
+
+import java.util.List;
+
+import eds.cristian.domain.Task;
+
+public interface TaskService {
+	
+	Task findById(Long id);
+	
+	Task create(Task taskToCreate);
+	
+	List<Task> findAll();
+
+}
---
+++ src/main/java/eds/cristian/service/impl/TaskServiceImpl.java
@@ -0,0 +1,32 @@
+package eds.cristian.service.impl;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import eds.cristian.domain.Task;
+import eds.cristian.domain.repository.ITaskRepository;
+import eds.cristian.service.TaskService;
+
+public class TaskServiceImpl implements TaskService {
+	
+	@Autowired
+	private ITaskRepository taskRepository;
+
+	@Override
+	public Task findById(Long id) {
+		return taskRepository.findById(id).orElseThrow(NoSuchElementException::new);
+	}
+
+	@Override
+	public Task create(Task taskToCreate) {
+		return taskRepository.save(taskToCreate);
+	}
+
+	@Override
+	public List<Task> findAll() {
+		return taskRepository.findAll();
+	}
+
+}
